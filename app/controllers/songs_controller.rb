768d903d873af277a9fcb4affee5f0f421c40b3a
Ashley Schultz <ashleyeschultz@gmail.com>
2018-06-29 00:54:36 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,6 +1,5 @@
 class SongsController < ApplicationController
 
-<<<<<<< HEAD
   def index
     @songs = Song.all
   end
@@ -9,10 +8,10 @@ class SongsController < ApplicationController
     @song = Song.find(params[:id])
     @artist = Artist.find(@song.artist_id)
     @genre = Genre.find(@song.genre_id)
-=======
+  end
+  
   def show
     @song = Song.find(params[:id])
->>>>>>> ef99eb6127a2f12d6b2fdbbea0196c88aa9b61a5
   end
 
   def new
@@ -20,12 +19,9 @@ class SongsController < ApplicationController
   end
 
   def create
-<<<<<<< HEAD
     @song = Song.create(params.require(:song).permit(:name, :artist, :genre))
     redirect_to song_path(@song)
-=======
     @song = Song.create(params.require(:songs).permit(:name, :artist_id, :genre_id))
->>>>>>> ef99eb6127a2f12d6b2fdbbea0196c88aa9b61a5
   end
 
   def edit
@@ -34,12 +30,8 @@ class SongsController < ApplicationController
 
   def update
     @song = Song.find(params[:id])
-<<<<<<< HEAD
     @song = Song.update(params.require(:song).permit(:name))
     redirect_to song_path(@song)
-=======
-    @song = Song.update(params.require(:songs).permit(:name, :artist_id, :genre_id))
->>>>>>> ef99eb6127a2f12d6b2fdbbea0196c88aa9b61a5
   end
 
 end
